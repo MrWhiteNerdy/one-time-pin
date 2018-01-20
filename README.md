4a50a1746e39164e6471956836174b5a430ac62c
Joseph Davis <jnd13160@ucmo.edu>
2018-01-20 09:44:41 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Read this first.
+# One Time Pin
+
+Capstone project for CS4920 Senior Project at UCM.
+
+Generates a unique one-time random 6-digit PIN for a given account.
+
+## Technology Stack
+
+* Java 8
+* Spring Framework
+* Maven
+* MySQL
+
+## Contributors
+
+* Nathan Boehning
+* Joseph Davis
+* Jansen Morby
---
+++ src/main/java/edu/ucmo/mathcs/onetimepin/OneTimePinApplication.java
@@ -0,0 +1,12 @@
+package edu.ucmo.mathcs.onetimepin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTimePinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTimePinApplication.class, args);
+	}
+}
